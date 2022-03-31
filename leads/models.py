a696091d7d47d02b28727eef191b1fba78b87232
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-31 15:12:53 +0500

    Lead models created

---
+++ leads/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Lead(models.Model):
+    ismi = models.CharField(max_length=20)
+    familiyasi = models.CharField(max_length=20)
+    yoshi = models.IntegerField(default=0)
+    
