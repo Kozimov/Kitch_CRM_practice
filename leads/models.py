222066d822403a882d42bf5e087437d1ffc2e943
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-31 15:41:44 +0500

    User abstrct

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
     ismi = models.CharField(max_length=20)
     familiyasi = models.CharField(max_length=20)
     yoshi = models.IntegerField(default=0)
-    
+
