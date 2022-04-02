02f2dd0c1aefbfe6b362659c72fe3670d1cb5e8f
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-02 11:43:04 +0500

    404 not found

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from . import models
 
 def leads_lists(request):
@@ -8,3 +9,8 @@ def leads_lists(request):
         "leads": leads
     }
     return render(request, 'leads_lists.html', context)
+
+
+def leads_detail(request, pk):
+    lead = get_object_or_404(models.Lead, id=pk)
+    return render(request, 'details.html')
