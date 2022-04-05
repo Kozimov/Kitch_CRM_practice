446863fd4e3e27ddbd2bc313c25ee04cb3a236f0
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-05 12:06:40 +0500

    front new

--- leads/urls.py
+++ leads/urls.py
@@ -7,7 +7,7 @@ app_name = "leads"
 urlpatterns = [
     path('', leads_lists),
     path('<int:pk>/', lead_detail, name="detallar"),
-    path('<int:pk>/update', lead_update, name="update"),
+    path('<int:pk>/update_detail', lead_update, name="update"),
     path('<int:pk>/delete', lead_delete, name="delete"),
-    path('create-leads/', lead_create, name="create"),
+    path('create-leads/', lead_create, name="lead-create"),
 ]
