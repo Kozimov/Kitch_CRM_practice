eafc976b28a32f454ffad7fda42ff063b60ff5c5
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-06 09:34:58 +0500

    home page and Lists urls

--- kitchencrm/urls.py
+++ kitchencrm/urls.py
@@ -1,7 +1,10 @@
+import imp
 from django.contrib import admin
 from django.urls import path, include
+from leads.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view()),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
