d456f22e81c42d4d1377eec5fa7bd4ce0cdc73a9
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-07 11:33:22 +0500

    login page loginview

--- kitchencrm/settings.py
+++ kitchencrm/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+LOGIN_REDIRECT_URL = "/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
