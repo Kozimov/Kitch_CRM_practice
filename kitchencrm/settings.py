69979c998d55543d86ef6281be8d9b0fe4669749
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-07 12:11:13 +0500

    login ended

--- kitchencrm/settings.py
+++ kitchencrm/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- kitchencrm/urls.py
+++ kitchencrm/urls.py
@@ -3,11 +3,12 @@ import django
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import HomeView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view()),
     path('leads/', include('leads.urls', namespace="leads")),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
