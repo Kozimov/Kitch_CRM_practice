c3cb601210559bdeaabf65b524a7ac78921dc98a
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-14 17:02:07 +0500

    pass res complete

--- sucrm/urls.py
+++ sucrm/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import (
     LogoutView, 
     PasswordResetView, 
     PasswordResetDoneView ,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
     )
 from django.urls import path, include
 from leads.views import HomeView, SigupView
@@ -16,7 +18,9 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace="agents")),
     path('signup/', SigupView.as_view(), name="signup"),
     path('login/', LoginView.as_view(), name="login"),
-    path('password_reset_view/', PasswordResetView.as_view(), name="password_reset_view"),
-    path('password_reset_done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset-view/', PasswordResetView.as_view(), name="password_reset_view"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('logout/', LogoutView.as_view(), name="logout")
 ]
