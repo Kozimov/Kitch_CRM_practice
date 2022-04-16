dbff5b912fd350e05957d70d008c70a057858949
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-16 10:41:35 +0500

    agentni aniqlash run

--- leads/forms.py
+++ leads/forms.py
@@ -29,10 +29,12 @@ class NewUserForm(UserCreationForm):
 
 
 class AssignAgentForm(forms.Form):
-    agent = forms.ModelChoiceField(queryset=Agent.objects.null())
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
 
     def __init__(self, *args, **kwargs):
         request = kwargs.pop("request")
         agents = Agent.objects.filter(organisation=request.user.userprofile)
         super(AssignAgentForm, self).__init__(*args, **kwargs)
         self.fields["agent"].queryset = agents
+
+    
